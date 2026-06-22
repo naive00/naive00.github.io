66db29f9fdb3462cad8860f2652c022b46106597
jhon brayan <112669803+naive00@users.noreply.github.com>
2026-06-21 20:38:18 -0500

    primercomit

---
+++ jose.js
@@ -0,0 +1,32 @@
+
+document.querySelectorAll('.faq-question').forEach(button => {
+
+    button.addEventListener('click', () => {
+
+        const item = button.parentElement;
+
+        item.classList.toggle('active');
+
+    });
+
+});
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-links a")
+.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
